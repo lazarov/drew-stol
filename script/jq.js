38e2e4004521c4e3cb671a441b6524862a4dd73e
la <lazarov@gazeta.pl>
2015-07-14 15:57:29 +0200

    added fade in

--- script/jq.js
+++ script/jq.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
-  $("header").hide(0).delay(0).fadeIn(2500)
+  $("*").dblclick(function(e) {
+       e.preventDefault();
+  });
+  $("header").hide(0).delay(0).fadeIn(2500);
+  $("ul").show('slide', {direction: 'right'}, 1500);
   $("ul.nav li a").click(function () {
      $("header").hide( 1000 );
   });
   $('ul.nav li a').click(function() {
 			var page = $(this).attr('href');
-			$('#content').load('subs/' + page + '.html');
+			$('#content').load('subs/' + page + '.html').hide(0).delay(0).fadeIn(1500);
 			return false;
 	});
 });
