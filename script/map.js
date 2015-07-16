4f610109f92b4a1d3cf24e067bef055d7835d6fa
lazarov <lazarov@gazeta.pl>
2015-07-16 17:31:33 +0200

    google maps added

--- script/map.js
+++ script/map.js
@@ -1,19 +1,16 @@
-function initialize()
-{
-  var mapProp = {
-    center: new google.maps.LatLng(51.508742,-0.120850),
-    zoom:7,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  var map = new google.maps.Map(document.getElementById("map-canvas"),mapProp);
+function initialize() {
+  var myLatlng = new google.maps.LatLng(49.720550, 21.329594);
+  var mapOptions = {
+    zoom: 11,
+    center: myLatlng
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'DREW-STOL, 38-243 Harklowa 108'
+  });
 }
 
-function loadScript()
-{
-  var script = document.createElement("script");
-  script.type = "text/javascript";
-  script.src = "http://maps.googleapis.com/maps/api/js?key=&sensor=false&callback=initialize";
-  document.body.appendChild(script);
-}
-
-window.onload = loadScript;
+window.onload = initialize();
